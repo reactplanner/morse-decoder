0f244c761918e95be3605912c6723060bc8b6334
OLG <lastdayoflife2@gmail.com>
2020-10-16 23:20:31 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -37,8 +37,53 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+	// write your solution here
+	console.log(expr.length);
+	let str = '';
+
+	let res = [];
+	let temp = '';
+	for (let i = 0; i <= expr.length; i++) {
+		if (i % 10 == 0 && i !== 0) {
+			res.push(temp);
+			temp = '';
+			temp = expr.charAt(i);
+		} else {
+			temp += expr.charAt(i);
+		}
+	}
+	res.map((elem, index) => {
+		let strTemp = ''
+		for (let i = 0; i <= elem.length; i++) {
+			let substr = elem.substring(i, i + 2)
+			i++
+			switch (substr) {
+				case '00':
+					strTemp += ''
+					break;
+				case '10':
+					strTemp += '.'
+					break;
+				case '11':
+					strTemp += '-'
+					break;
+				case '**':
+					strTemp += '**'
+					break;
+				default:
+					break;
+			}
+		}
+		if (strTemp === '**********') {
+			str += ' '
+		} else {
+			str += MORSE_TABLE[strTemp]
+		}
+	})
+	return str
+
 }
 
 module.exports = {
